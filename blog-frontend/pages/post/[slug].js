622344b664b5a134daeeb0613d28090a71af1115
Miguel Chiau <chiau.miguel@gmail.com>
2021-02-24 17:23:39 -0800

    first blog post

--- blog-frontend/pages/post/[slug].js
+++ blog-frontend/pages/post/[slug].js
@@ -1,7 +1,71 @@
-export const Post = () => {
-    return <> I'm a Post 4 Test </>
+import styles from "../../styles/Post.module.css"
+import imageUrlBuilder from "@sanity/image-url"
+import {useState, useEffect} from "react"
+import BlockContent from "@sanity/block-content-to-react"
+
+
+
+export const Post = ({ title, body, image}) => {
+const [imageUrl, setImageUrl] = useState('')
+
+useEffect(() => {
+    const imgBuilder = imageUrlBuilder({
+        projectId: "56rd5uw6",
+        dataset: "production"
+    })
+
+    setImageUrl(imgBuilder.image(image))
+    
+}, [image])
+
+    return (
+        <div>
+            <div className={styles.main}>
+                <h1>
+                    {title}
+                </h1>
+                {imageUrl && <img className={styles.mainImage} src={imageUrl}/>}
+
+                <div className={styles.body}>
+                    <BlockContent blocks={body}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
+//Make the request for the info on the dashboard of Sanity
 
+export const getServerSideProps = async pageContext => {
+    const pageSlug = pageContext.query.slug
+    
+    if(!pageSlug) {
+        return {
+            notFound: true
+        }
+    }
+
+    const query = encodeURIComponent(`*[ _type == "post" && slug.current == "${pageSlug}" ]`)
+
+    const url = `https://56rd5uw6.api.sanity.io/v1/data/query/production?query=${query}`
+
+    const result = await fetch(url).then(res => res.json())
+    const post = result.result[0]
+
+    if(!post) {
+        return {
+            notFound: true
+        }
+    }
+     else {
+            return {
+                props: {
+                    body: post.body,
+                    title: post.title,
+                    image: post.mainImage,
+                }
+            }
+        }
+}
 
 export default Post
